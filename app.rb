eeb36329315e0fc75692c01c15ca0e8c8a3f77e8
Alessandro Drappa <drappa.alessandro@gmail.com>
2017-05-01 17:17:30 +0200

    Laster version

--- app.rb
+++ app.rb
@@ -5,6 +5,9 @@ require "better_errors"
 require_relative 'cookbook'    # You need to create this file!
 require_relative "recipes.rb"
 require 'csv'
+require "fatsecret"
+FatSecret.init("3808973e9a654b9f8241056aad578a91","0d11a349851d4a05a13534314d85d1db")
+
 
 csv_file   = File.join(__dir__, 'recipes.csv')
 cookbook   = Cookbook.new(csv_file)
@@ -52,7 +55,45 @@ get '/destroy/:i' do
   redirect "/list"
 end
 
+get '/search' do
+  if !params["q"].nil?
+    food = params["q"]
+    # We are searching for a recipe containing the food
+    search_result = FatSecret.search_recipes(food, 5)
+    # We are storing the recipe description withpa an index number
+    id_index = {}
+    @q = food
+    search_result["recipes"]["recipe"].each_with_index do |recipe, index|
+       id_index[index] = recipe["recipe_id"]
+      end
+    # We are storing the descriptions into an array that we will display to the user
+    descriptions = search_result["recipes"]["recipe"].map do |recipe|
+      recipe["recipe_description"]
+      end
+    @recipe_image = search_result["recipes"]["recipe"].map do |recipe|
+        recipe["recipe_image"]
+      end
 
+    @id_index = id_index
+    @description = descriptions
+    # We are asking the user for the index of the recipe he wants and makes it corresponds to a recipe ID
+      if !params["id"].nil?
+        choosen_recipe = FatSecret.recipe(id_index[params["id"].to_i])
+        # # We are creating the new recipe
+        # calories = choosen_recipe["recipe"]["serving_sizes"]["serving"]["calories"]
+        recipe_name = choosen_recipe["recipe"]["recipe_name"]
+        description = choosen_recipe["recipe"]["recipe_description"]
+        new_recipe = Recipe.new(recipe_name, description )
+        # Add the new recipe to the repo
+        cookbook.add_recipe(new_recipe)
+        redirect "/list"
+      end
+  else
+    @id_index = []
+    @description = []
+  end
+  erb :search
+end
 class Cookbook # CookbookRepository
   def initialize(csv_file_path)
     @csv_file_path = csv_file_path
@@ -111,8 +152,27 @@ class Recipe
 end
 
 
-
-
-
-
-
+  # def searchrecipe
+  #   # We ask the user which food does he want in its recipe
+  #   food = @view.ask_user_for_food
+  #   # We are searching for a recipe containing the food
+  #   search_result = FatSecret.search_recipes(food, 5)
+  #   # We are storing the recipe description with an index number
+  #   id_index = {}
+  #   search_result["recipes"]["recipe"].each_with_index do |recipe, index|
+  #      id_index[index + 1] = recipe["recipe_id"]
+  #     end
+  #   # We are storing the descriptions into an array that we will display to the user
+  #   descriptions = search_result["recipes"]["recipe"].map do |recipe|
+  #     recipe["recipe_description"]
+  #     end
+  #   # We are asking the user for the index of the recipe he wants and makes it corresponds to a recipe ID
+  #   choosen_recipe = FatSecret.recipe(id_index[@view.user_select_recipe(descriptions)])
+  #   # We are creating the new recipe
+  #   @calories = choosen_recipe["recipe"]["serving_sizes"]["serving"]["calories"]
+  #   recipe_name = choosen_recipe["recipe"]["recipe_name"]
+  #   description = choosen_recipe["recipe"]["recipe_description"]
+  #   new_recipe = Recipe.new(recipe_name, description )
+  #   # Add the new recipe to the repo
+  #   @cookbook.add_recipe(new_recipe)
+  # end
